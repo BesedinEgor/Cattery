013ff016a35f6f5838965dee31c0e4d792d90b1a
BesedinEgor <74902393+BesedinEgor@users.noreply.github.com>
2021-09-01 20:50:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.pets__slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 1200,
+        autoplay: false
+    });
+
+    // $('.team__slider').slick({
+    //     arrows: true,
+    //     // fade: true,
+    //     speed: 1200,
+    //     autoplay: false
+    // });
+});
