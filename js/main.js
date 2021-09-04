334ce330584c7be601152dac22685ee58be782c8
BesedinEgor <74902393+BesedinEgor@users.noreply.github.com>
2021-09-04 15:54:00 +0300

    js fix section FAQ

--- js/main.js
+++ js/main.js
@@ -13,4 +13,14 @@ $(document).ready(function () {
         speed: 1200,
         autoplay: false
     });
+    const listItems = document.querySelectorAll('.faq__items');
+    const listBtn = document.querySelectorAll('.list__btn');
+
+    listItems.forEach(function (item) {
+        let btn = item.querySelector('.list__btn')
+        btn.addEventListener('click', function () {
+            item.classList.toggle('faq__items-active')
+            btn.classList.toggle('list__btn-active')
+        })
+    })
 });
